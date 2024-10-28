dc97f4adc1de2d8b06253da856d4053a1aa42dc7
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-28 15:48:58 +0100

    add function to add new card

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+function createCard(nameSurname, role, email, img){
+  // let add= document.getElementById("add");
+  add.innerHTML+=
+  `<div class="col-4">
+    <div class="card mb-3 bg-black text-white" id="card">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img id="image5" src = ${img} class="img-fluid rounded-start max h-100" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title" id="nameSurname5">${nameSurname}</h5>
+            <p class="card-text" id="mansion5">${role}</p>
+            <p><small class="text-body-secondary"><a class="link-opacity-100-hover" id="link5" href="#">${email}</a></small></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`
+}
+
 const teamMembers = [
   {
     name: "Marco Bianchi",
@@ -44,5 +65,24 @@ for(let i = 0; i<teamMembers.length ; i++){
   document.getElementById("link"+i).innerText = teamMembers[i].email;
 }
 
+const form = document.getElementById("form");
 
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
+  let nameSurname = document.getElementById("nameSurname").value;
+  let role = document.getElementById("role").value;
+  let email = document.getElementById("email").value; 
+  let img = document.getElementById("img").value; 
+
+  console.log(nameSurname, role, email, img)
+  let objApp = {
+    name: nameSurname,
+    role: role,
+    email: email,
+    img: img
+  }
+  createCard(nameSurname, role, email, img);
+  teamMembers.push(objApp)
+  console.log(teamMembers)
+})
